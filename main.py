407aa4f018941bf761a219a661a1ba3206f8a892
CAL <chiaal@hotmail.com>
2022-03-27 15:27:31 +0800

    Working with Class

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+print (format(__name__))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class new_car:
+    '''initiate a new car'''
+    def __init__(self,a,b,c):
+        self.brand = a
+        self.out_year = b
+        self.manufacturer = c
 
+a = new_car("Altis", 1992, "Toyata")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(new_car.__doc__) #print out the comments
+print(a.brand)
 
+record=[]
+record.append(a)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(record[0].brand)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
